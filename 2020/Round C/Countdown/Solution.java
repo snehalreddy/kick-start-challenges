5c09850a7f6d47dd5cf6a6f75b77c8dd0654fb23
snehalreddy <syelmat@gmail.com>
2020-12-20 12:50:36 +0530

    Moved Bike tour to Round B folder

---
+++ 2020/Round C/Countdown/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int a[], ans = 0;
+        for (int i = 0; i < T; i++) {
+
+            a = new int[N];
+
+            for (int j = 0; j < N; j++) {
+                a[j] = sc.nextInt();
+            }
+
+            // some code here
+
+            System.out.println("Case #" + (i + 1) + ": " + ans);
+
+        }
+    }
+}
